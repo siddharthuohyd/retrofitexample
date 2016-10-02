4089ac4ab063f92508d4d4facf601bf5b06d6a73
siddharth <siddharthuohyd>
2016-10-02 21:19:50 +0530

    commit by siddhrath

--- app/src/main/java/com/example/sid/retrofitexample/MainActivity.java
+++ app/src/main/java/com/example/sid/retrofitexample/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
